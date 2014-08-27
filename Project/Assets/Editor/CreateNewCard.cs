028c44cee237c5b1caa943e4a0a7f552234e72f1
tdroesch <tdroesch13@gmail.com>
2014-08-27 14:16:07 -0700

    Started Card Creator
    Started an editor class to assist with card creation.

---
+++ Project/Assets/Editor/CreateNewCard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public static class CreateNewCard {
+
+	[MenuItem("GameObject/Dreamscape/Create Card")]
+	static void CreateCard()
+	{
+		Debug.Log("Card");
+	}
+}
