e9f20de341b199b79f83a9fbe9ae98c06ceaf7b3
dorichan <dori.p.chan@gmail.com>
2014-08-26 20:38:38 -0700

    Added basic scripts
    They're empty. This is also a test.

---
+++ Project/Assets/Scripts/Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour 
+{
+	public Player player1;
+	public Player player2;
+	public Board board;
+
+	public void FSM()
+	{
+
+	}
+
+	void OnGUI()
+	{
+
+	}
+}
---
+++ Project/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public Hand hand;
+	public Deck deck;
+	public Field field;
+	public Subconscious discard;
+	public IController controller;
+
+
+}
