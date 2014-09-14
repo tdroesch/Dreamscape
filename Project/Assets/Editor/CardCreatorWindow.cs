626076757a7871a3ecbbd3964111f17816732b97
tdroesch <tdroesch13@gmail.com>
2014-09-13 18:54:45 -0700

    Edit Card Creator
    Learned more about editor scripts and updated the card creator.

---
+++ Project/Assets/Editor/CardCreatorWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class CardCreatorWindow : EditorWindow {
+
+	[MenuItem("GameObject/Dreamscape/Create Card")]
+	static void Init ()
+	{
+		CardCreatorWindow cardWindow = new CardCreatorWindow();
+		cardWindow.position = new Rect (50, 50, 200, 200);
+		cardWindow.title = "Card Creator";
+		cardWindow.Show ();
+	}
+
+	static void OnGUI()
+	{
+
+	}
+}
