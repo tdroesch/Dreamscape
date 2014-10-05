bf8df9686ffd6a8ce828396f46b748f3a5dc04ff
tdroesch <tdroesch13@gmail.com>
2014-10-05 04:18:31 -0700

    Gave up on Card Creator

--- Project/Assets/Editor/CardCreatorWindow.cs
+++ Project/Assets/Editor/CardCreatorWindow.cs
@@ -7,10 +7,14 @@ public class CardCreatorWindow : EditorWindow {
 	[MenuItem("GameObject/Dreamscape/Create Card")]
 	static void Init ()
 	{
-		CardCreatorWindow cardWindow = new CardCreatorWindow();
+		CardCreatorWindow cardWindow = ScriptableObject.CreateInstance<CardCreatorWindow>();
 		cardWindow.position = new Rect (50, 50, 200, 200);
 		cardWindow.title = "Card Creator";
 		cardWindow.Show ();
+		var guids = AssetDatabase.FindAssets("t:Object");
+		foreach (var guid in guids){
+			Debug.Log(AssetDatabase.GUIDToAssetPath(guid));
+		}
 	}
 
 	static void OnGUI()
