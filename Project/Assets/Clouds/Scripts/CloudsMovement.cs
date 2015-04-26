339d51a086dd0bff37e85089429efb992831aec6
dorichan <dori.p.chan@gmail.com>
2015-04-25 23:52:01 -0700

    -Cloud and fog prefabs + test scene, textures, materials, and script

---
+++ Project/Assets/Clouds/Scripts/CloudsMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsMovement : MonoBehaviour 
+{
+	public float speed;
+
+	void Start()
+	{
+		speed = 2.5f;
+	}
+
+	void Update()
+	{
+		gameObject.transform.Rotate (Vector3.up * speed * Time.deltaTime);
+	}
+}
