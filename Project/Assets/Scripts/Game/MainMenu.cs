591727794c6bc3098873a482e93a7b24d9f1dd62
Justin Collins <justcollins@comcast.net>
2015-04-26 21:08:43 -0700

    Main Menu Networking
    Added Andrew's Network Manager script to work with the Main Menu.
    The New Game button no longer loads a new scene, it instead hides the
    main menu and brings up the Network Manager's buttons to connect to the
    servers.

--- Project/Assets/Scripts/Game/MainMenu.cs
+++ Project/Assets/Scripts/Game/MainMenu.cs
@@ -5,16 +5,11 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-	//new game function, loads a new level
-	public void NewGame()
-	{
-		Application.LoadLevel (1);
-	}
-
 	//card catalog function, loads the card catalog level
+	//not sure what the card catalog's level name is going to be at the time when this was written
 	public void CardCatalog ()
 	{
-		Application.LoadLevel (2);
+		Application.LoadLevel ("Place Holder Level");
 	}
 
 	//quit game function, exits the application
