4fb20b5c137290ad9193b0337e3acef65b93c5ef
AndrewXA9 <andrewxa9@yahoo.com>
2015-05-16 21:24:40 -0700

    iunno

--- Project/Assets/CardEditor/CardEditor.cs
+++ Project/Assets/CardEditor/CardEditor.cs
@@ -4,13 +4,13 @@ using System.Xml;
 
 public class CardEditor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public Vector2 scrollPosition = Vector2.zero;
+	void OnGUI() {
+		scrollPosition = GUI.BeginScrollView(new Rect(10, 300, 100, 100), scrollPosition, new Rect(0, 0, 220, 200));
+		GUI.Button(new Rect(0, 0, 100, 20), "Top-left");
+		GUI.Button(new Rect(120, 0, 100, 20), "Top-right");
+		GUI.Button(new Rect(0, 180, 100, 20), "Bottom-left");
+		GUI.Button(new Rect(120, 180, 100, 20), "Bottom-right");
+		GUI.EndScrollView();
 	}
 }
